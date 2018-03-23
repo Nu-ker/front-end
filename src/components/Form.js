79108cdc2386c467cb5ba52014161cacd0e04ecf
ariefmanda <arief.manda57@gmail.com>
2018-03-23 18:11:15 +0700

    formregister

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,108 @@
 import React from 'react';
-import { StyleSheet, Text, ScrollView, ImageBackground, TouchableHighlight, View, Image, TextInput } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  ScrollView, 
+  ImageBackground, 
+  TouchableHighlight, 
+  View, 
+  Image, 
+  TextInput,
+  Picker 
+} from 'react-native';
 import * as Animatable from 'react-native-animatable';
+import axios from 'axios'
 
 export default class Home extends React.Component {
+  constructor() {
+    super()
+    this.state={
+      weight:'',
+      height:'',
+      age:'',
+      sex:'',
+      activity:'',
+      weightTarget:''
+    }
+  }
   componentWillMount(){
     console.log(this.props.navigation.state.params.dataLogin);
+  }
+  handleSubmit= async ()=>{
+    console.log(this.state);
+    let user = await axios.post('https://us-central1-nu-ker-fox.cloudfunctions.net/User',{
+      photoUrl:this.props.navigation.state.params.dataLogin.photoUrl,
+      email:this.props.navigation.state.params.dataLogin.email,
+      name: this.props.navigation.state.params.dataLogin.name,
+      weight: this.state.weight,
+      height: this.state.height,
+      age: this.state.age,
+      sex: this.state.sex,
+      activity: this.state.activity,
+    },{
+      headers:{
+        uid:this.props.navigation.state.params.dataLogin.id
+      }
+    })
+    if(user.data.msg === "SUCCESS POST"){
+      this.props.navigation.navigate('DashBoard')
+    }
   }
   render () {
     return (
       <ImageBackground style={styles.back} source={require('../assets/tes.jpg')}>
         <Text style={styles.judul}>Please Fill The Form</Text>
         <ScrollView style={styles.wrap}>
-          <TextInput placeholder="Your Weight (kg)" keyboardType="default" style={styles.input}></TextInput>
-          <TextInput placeholder="Your Height (cm)" keyboardType="default" style={styles.input}></TextInput>
-          <TextInput placeholder="Your Gender" keyboardType="default" style={styles.input}></TextInput>
-          <TextInput placeholder="Your Age" keyboardType="default" style={styles.input}></TextInput>
-          <TextInput placeholder="Weight Target" keyboardType="default" style={styles.input}></TextInput>
-          <TouchableHighlight onPress={() => this.props.navigation.navigate('DashBoard')} style={styles.next}>
+          <TextInput placeholder="Your age" keyboardType="default" style={styles.input}
+          onChangeText={(age) => this.setState({
+            ...this.state,
+            age
+          })}
+          value={this.state.age}/>
+          <TextInput placeholder="Your Weight (kg)" keyboardType="default" style={styles.input}
+          onChangeText={(weight) => this.setState({
+            ...this.state,
+            weight
+          })}
+          value={this.state.weight}/>
+          <TextInput placeholder="Your Height (cm)" keyboardType="default" style={styles.input}
+          onChangeText={(height) => this.setState({
+            ...this.state,
+            height
+          })}
+          value={this.state.height}/>
+          <Picker
+            style={{width: '90%', alignSelf:'center'}}
+            selectedValue={this.state.sex}
+            onValueChange={(itemValue, itemIndex) => this.setState({
+              ...this.state,
+              sex:itemValue})}>
+            <Picker.Item label="Male" value="male" />
+            <Picker.Item label="Female" value="female" />
+          </Picker>
+
+          <TextInput placeholder="Weight Target" keyboardType="default" style={styles.input}
+          onChangeText={(weightTarget) => this.setState({
+            ...this.state,
+            weightTarget
+          })}
+          value={this.state.weightTarget}/>
+
+          <Picker
+            style={{width: '90%', alignSelf:'center'}}
+            selectedValue={this.state.activity}
+            onValueChange={(itemValue, itemIndex) => this.setState({
+              ...this.state,
+              activity:itemValue
+            })}>
+            <Picker.Item label="Sedentary" value="1.2" />
+            <Picker.Item label="Lightly Active" value="1.375" />
+            <Picker.Item label="Active" value="1.55" />
+            <Picker.Item label="Very Active" value=" 1.725" />
+            <Picker.Item label="Extreme" value="1.9" />
+          </Picker>
+
+          <TouchableHighlight onPress={() => this.handleSubmit()} style={styles.next}>
             <Text style={styles.text}>NEXT</Text>
           </TouchableHighlight>
         </ScrollView>
